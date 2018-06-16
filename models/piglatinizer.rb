8528073648386d05ae692ffd168a4cc8c7fa3f5d
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-16 23:42:28 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,6 +16,10 @@ class PigLatinizer
         first_two_letters_plus_ay = word[0..1].concat("ay")
         word[0..1] = ""
         @sentence = @sentence.concat("#{word.concat(first_two_letters_plus_ay)} ")
+      elsif !word[0].match(/[aeiouAEIOU]/) && !word[1].match(/[aeiouAEIOU]/)
+        first_two_letters_plus_ay = word[0..1].concat("ay")
+        word[0..1] = ""
+        @sentence = @sentence.concat("#{word.concat(first_two_letters_plus_ay)} ")
       else
         first_letter_plus_ay = word[0].concat("ay")
         word[0] = ""

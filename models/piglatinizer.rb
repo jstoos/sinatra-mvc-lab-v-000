b6d98657c40d8b4a1bd293d67d9caed9fc8d76c4
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-16 22:32:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -15,7 +15,8 @@ class PigLatinizer
       else
         first_letter = word[0]
         word[0] = ""
-        ending = "#{word.concat('#{first_letter}ay) '}"
+        ending = first_letter"ay"
+        #{word.concat('#{first_letter}ay) '}
         @sentence.concat(ending)
       end
     end

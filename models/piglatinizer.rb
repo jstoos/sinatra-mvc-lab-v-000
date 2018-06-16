6e0e77bca5a56071c54746b6d8560837a8986c2f
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-16 23:45:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,14 +12,15 @@ class PigLatinizer
     @text.collect do |word|
       if word[0].match(/[aeiouAEIOU]/)
         @sentence = @sentence.concat("#{word.concat('way')} ")
-      elsif !word[0].match(/[aeiouAEIOU]/) && !word[1].match(/[aeiouAEIOU]/)
-        first_two_letters_plus_ay = word[0..1].concat("ay")
-        word[0..1] = ""
-        @sentence = @sentence.concat("#{word.concat(first_two_letters_plus_ay)} ")
+    
       elsif !word[0].match(/[aeiouAEIOU]/) && !word[1].match(/[aeiouAEIOU]/) && !word[2].match(/[aeiouAEIOU]/)
         first_three_letters_plus_ay = word[0..2].concat("ay")
         word[0..2] = ""
         @sentence = @sentence.concat("#{word.concat(first_three_letters_plus_ay)} ")
+      elsif !word[0].match(/[aeiouAEIOU]/) && !word[1].match(/[aeiouAEIOU]/)
+        first_two_letters_plus_ay = word[0..1].concat("ay")
+        word[0..1] = ""
+        @sentence = @sentence.concat("#{word.concat(first_two_letters_plus_ay)} ")
       else
         first_letter_plus_ay = word[0].concat("ay")
         word[0] = ""

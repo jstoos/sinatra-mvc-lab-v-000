f2f1366339e69fe0e743feefa8da2e4b6af3a33f
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-16 22:10:47 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+  attr_reader :text, :sentence
+
+  @text = []
+  @sentence = ""
+
+  def initialize
+  end
+
+  def piglatinize(text)
+    @text = text.split
+    @text.collect do |word|
+      if word[0].match(/[aeiou]/)
+        @sentence = @sentence.concat("x")
+        #@sentence = @sentence.concat("#{word.concat('way')} ")
+      # else
+        # first_letter = word[0]
+        # word[0] = ""
+        # @sentence.concat("#{word.concat('#{first_letter}ay)'} ")
+      end
+    end
+    binding.pry
+  end
+
+end
